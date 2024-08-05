5ccfbc61ef00e6add41ac749bd122804e4b8e62d
KAA-MB <mbeckmann@kaadesigngroup.com>
2024-08-05 18:45:02 -0400

    Add files via upload
    Number Zones sequentially starting from "First Zone" (a custom property), and proceeding by closest distance from the previous zone numbered. If there's a selection, the script uses only selected zones; otherwise it uses all zones in project.

---
+++ Number_Zones_byDistanceFromPrevious_v1.py
@@ -0,0 +1,230 @@
+######################################### General Info #########################################
+# Written by: Jessica Wood  w/ Meghan Beckmann, for KAA Design Group                           #
+# Date Created: 09/2022                                                                        #
+#                                                                                              #
+# Description:                                                                                 #
+# This script generates unique ordered zone numbers. Sets the zone number                      #
+# property value for all selected zones in the project. If no zones are selected, the          #
+# script will set the zone number property value for all zones in the project. The             #
+# script starts sorting the zones from the user-defined zone property “First_Zone”             #
+# and orders it by closest distance to the current zone being looked at.                       #
+################################################################################################
+
+
+
+
+############ Archicad Connection #############
+from archicad import ACConnection
+from typing import List, Tuple, Iterable
+from itertools import cycle
+import copy
+import math
+
+conn = ACConnection.connect()
+assert conn
+
+acc = conn.commands
+act = conn.types
+acu = conn.utilities
+##############################################
+
+
+
+
+############################ CONFIGURATION: By distance from Entry Zone ################################
+
+# Get the zones
+propertyId = acu.GetBuiltInPropertyId('Zone_ZoneNumber')
+propertyValueStringPrefix = ''
+allZoneElements = acc.GetElementsByType('Zone') # holds all zones
+selectedElements = acc.GetSelectedElements() # holds selected zones
+
+# Get property values of "Entry"
+entryPropertyId = acu.GetUserDefinedPropertyId("KAA Python", "First_Zone")
+entryPropertyIdArrayItem = [act.PropertyIdArrayItem(entryPropertyId)]
+
+
+###### Constant Values #####
+STORY_GROUPING_LIMIT = 1   #
+############################
+
+########################################################################################################
+
+
+
+
+############################################################################## FUNCTIONS ##################################################################################
+
+def GeneratePropertyValueString(storyIndex: int, elemIndex: int) -> str:
+    return f"{propertyValueStringPrefix}{storyIndex:1d}{elemIndex:02d}"
+
+def generatePropertyValue(storyIndex: int, elemIndex: int) -> act.NormalStringPropertyValue:
+    return act.NormalStringPropertyValue(GeneratePropertyValueString(storyIndex, elemIndex))
+
+
+
+def createClusters(positions: Iterable[float], limit: float) -> List[Tuple[float, float]]:
+  # Function: creates clusters based on zValues of all the Zones. Clusters represent stories
+
+  # sort Zvalues in ascending order
+    positions1 = sorted(positions) 
+
+    if len(positions1) == 0:
+        return []
+
+    clusters = []
+    posIter = iter(positions1)
+    firstPos = lastPos = next(posIter)
+
+    for pos in posIter:
+        if pos - lastPos <= limit:
+            lastPos = pos
+        else:
+            clusters.append((firstPos, lastPos))
+            firstPos = lastPos = pos
+
+    clusters.append((firstPos, lastPos))
+    return clusters
+
+
+
+def sortPositionsByDistance(positions1: Iterable[Tuple[float, float, float, float, float]], entryPosition: Tuple[float, float]) -> List[Tuple[float, float, float, float, float]]:
+    # function: takes positions and the position of the entry room and returns positions sorted by their distance from the Entry zone
+
+    sortedPos = []
+    sortedPos.append(entryPosition)
+
+    for i in range(len(positions1)-1):
+        closestPoint = sorted(positions1, key=lambda e: math.dist((e[1].boundingBox3D.xMin, e[1].boundingBox3D.yMin), (sortedPos[i][1].boundingBox3D.xMin, sortedPos[i][1].boundingBox3D.yMin)))
+
+        for x in range(len(closestPoint)):
+            if (not isInArray(closestPoint[x], sortedPos)):
+                sortedPos.append(closestPoint[x])
+                break
+
+    # return sorted positions
+    return sortedPos
+
+
+def isInArray(element: Tuple[act.ElementIdArrayItem, act.BoundingBox3D], sortedPositions: List[Tuple[float, float]]):
+    # Function: checks if the element has already been numbered
+    for i in range(len(sortedPositions)):
+        if (sortedPositions[i][0].elementId.guid == element[0].elementId.guid):
+            return True
+    return False
+
+################################################################################################################################################################################
+
+
+
+
+############################################################################ Begin Logic #######################################################################################
+
+# -- positions are based on zone stamp -- #
+
+# find bounding boxes of elements to get correct story zClusters      
+boundingBoxes = acc.Get3DBoundingBoxes(allZoneElements)
+elementBoundingBoxes = list(zip(allZoneElements, boundingBoxes))
+
+# story clusters: range of (zMin, zMax) that represent each story
+zClusters = createClusters((bb.boundingBox3D.zMin for bb in boundingBoxes), STORY_GROUPING_LIMIT)
+
+
+# check if there are any elements selected
+if (len(selectedElements) == 0):
+    selected = False
+    elements = acc.GetElementsByType('Zone')
+    selectedBoundingBoxes = acc.Get3DBoundingBoxes(elements)
+    selectedElementBoundingBoxes = list(zip(elements, boundingBoxes))
+else:
+    # find bounding boxes of selected zones  
+    selected =  True
+    elements = acc.GetSelectedElements() 
+    selectedBoundingBoxes = acc.Get3DBoundingBoxes(elements)
+    selectedElementBoundingBoxes = list(zip(elements, selectedBoundingBoxes))
+
+
+storyIndex = 0
+elemPropertyValues = []
+
+
+# Iterate through each story
+for (zMin, zMax) in zClusters:
+
+    elemIndex = 1 # Counter to keep track of element number
+
+
+    # find zones on the current story from all zones
+    elemsOnStory = [e for e in elementBoundingBoxes if zMin <= e[1].boundingBox3D.zMin <= zMax] # has coordinates and guid
+    storyElems = [e[0] for e in elemsOnStory] # only has guid
+
+
+    # check if selected zones are in current story
+    isSelectedOnCurrStory = False
+    for zone in storyElems:
+        for selZone in elements:
+            if (zone.elementId.guid == selZone.elementId.guid):
+                isSelectedOnCurrStory = True
+
+    # If selected zones are not on current story, skip numbering
+    if (not isSelectedOnCurrStory):
+        storyIndex += 1
+        continue
+
+
+    # find zones on the current story from selected zones
+    zonesOnStory = [e for e in selectedElementBoundingBoxes if zMin <= e[1].boundingBox3D.zMin <= zMax] # has coordinates and guid
+
+    #find entry zone on current story
+    elementsEntryVals = acc.GetPropertyValuesOfElements([e[0] for e in zonesOnStory], entryPropertyIdArrayItem)
+
+    entryElement = 0
+    entryElementIdx = 0
+    for i in range(0, len(elementsEntryVals)):
+        if (elementsEntryVals[i].propertyValues[0].propertyValue.value == True):
+            entryElement = elements[i]
+            entryElementIdx = i
+
+
+    if (entryElement == 0): # no entry element found on this floor! Inform the user and exit the script
+        print(f"No First_Zone found on {storyIndex} story. Ensure you have set an entry Zone for each story.")
+        exit(-1)
+
+    # sort current story zones by distance of entry room
+    sortedPos = sortPositionsByDistance(zonesOnStory, zonesOnStory[entryElementIdx]) 
+
+
+    # iterate sorted positions and map them to its given element
+    for (element, box) in sortedPos:
+        # map the positon to its given elementxMin
+        elem = [e for e in zonesOnStory if element.elementId.guid == e[0].elementId.guid]
+   
+        # Add new property value to the element
+        elemPropertyValues.append(act.ElementPropertyValue(
+               elem[0][0].elementId, propertyId, generatePropertyValue(storyIndex, elemIndex)))
+
+        # Increment element index for every Zone
+        elemIndex += 1
+    # Increment Story Index to keep track of what story is being numbered
+    storyIndex += 1
+
+# sets the property value of all the elements in the project
+acc.SetPropertyValuesOfElements(elemPropertyValues)
+
+#######################################################################################################################################################################################
+
+
+
+
+##################################################################### Print the result - Zone ID and Zone Number ######################################################################
+if (selected):
+    newValues = acc.GetPropertyValuesOfElements(elements, [propertyId])
+    elemAndValuePairs = [(elements[i].elementId.guid, v.propertyValue.value) for i in range(len(newValues)) for v in newValues[i].propertyValues]
+    for elemAndValuePair in sorted(elemAndValuePairs, key=lambda p: p[1]):
+        print(elemAndValuePair)
+else:
+    newValues = acc.GetPropertyValuesOfElements(allZoneElements, [propertyId])
+    elemAndValuePairs = [(allZoneElements[i].elementId.guid, v.propertyValue.value) for i in range(len(newValues)) for v in newValues[i].propertyValues]
+    for elemAndValuePair in sorted(elemAndValuePairs, key=lambda p: p[1]):
+        print(elemAndValuePair) 
+#######################################################################################################################################################################################
